468944b3d6883af0044185cecffb2c3a7f4622d7
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-08-10 16:35:31 +0530

    Added:CSV file if incorrect Returns a custom Exception

--- src/test/java/com/bridgelabz/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/CensusAnalyserTest.java
@@ -2,10 +2,12 @@ package com.bridgelabz;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
 
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\\\Users\\\\KVN\\\\IdeaProjects\\\\IndianStatesCensus\\\\src\\\\test\\\\resources\\\\IndiaStateCode.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCode.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -15,6 +17,18 @@ public class CensusAnalyserTest {
             Assert.assertEquals(37,numOfRecords);
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
         }
     }
 }
